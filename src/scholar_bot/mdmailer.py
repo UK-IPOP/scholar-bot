596fae1f245e0b6a113636701024ef2f510b7001
Nick Anthony <nanthony007@gmail.com>
2021-10-05 10:36:01 -0400

    Add template email.

--- src/scholar_bot/mdmailer.py
+++ src/scholar_bot/mdmailer.py
@@ -1,5 +1,5 @@
 import mdmail
-
+import utils
 
 with open("my-email.md") as f:
     email = f.read()
@@ -14,6 +14,35 @@ smtp = {
     "password": "yxoswzfpbihxrjoa",
 }
 
+data = utils.get_metrics_change("Chris Delcher")
+df = utils.load_data()
+row = df[df.name == "Chris Delcher"].iloc[0]
+
+
+email = f"""
+# Hello {data['name']}
+
+Today you are being emailed to update you on the power of Python and automation.
+
+This email contains your updated Google Scholar metrics and how they have changed over the past week.
+
+Hope you enjoy it! :) 
+
+To see your full GS Profile, click [here]({'https://scholar.google.com/citations?user=' + row.gs_id})
+
+| Metric | Change |
+| --- | --- |
+| h_index | {data['h_index']} |
+| i10_index | {data['i10_index']} |
+| Citations | {data['cited_by']} |
+| Publications | {data['pub_count']} |
+
+
+Please do not respond to this email as it is automated and unsupervised.
+
+Thank you.
+
+"""
 mdmail.send(
     email,
     subject="Sample Email",
